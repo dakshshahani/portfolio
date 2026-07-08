54f88ffafbe5a3b6ec64eab776ba480e6931bf0e
Daksh Shahani <dakshitshahani@gmail.com>
2026-07-08 03:30:43 -0700

    remove hero section and shader background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,22 @@
 "use client";
 
 import { DockDemo } from "@/components/animated-dock";
-import { Hero } from "@/app/sections/hero";
 import { InteractiveGridPatternDemo } from "@/app/sections/animated-hero";
 import { Projects } from "@/app/sections/projects";
 import { Experience } from "@/app/sections/experience";
 import { Contact } from "@/app/sections/contact";
 import { Footer } from "@/app/sections/footer";
 import { About } from "@/app/sections/about";
-import { ShaderBackground } from "@/components/ShaderBackground";
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 
 export default function HomePage() {
   return (
     <div className="relative flex min-h-screen flex-col overflow-hidden bg-background">
-      {/* 🔹 Shader layer */}
-      {/* <div className="hidden md:block">
-        <ShaderBackground />
-      </div> */}
-
       {/* 🔝 Site content */}
       <div className="relative z-10 flex flex-col">
         <DockDemo />
         <main className="flex-1">
           <InteractiveGridPatternDemo />
-          {/* <Hero /> */}
           <About />
           <Experience />
           <Projects />
